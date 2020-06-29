aa9c7d25927d8083a91e77a54e0d62bdc8b3ea70
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-06-29 13:32:07 -0400

    Finished assignment

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function addMeToEnd(arr) {
+  arr.push("Colin");
+}
 
+function addMeToStart(arr) {
+  arr.unshift("Colin");
+}
 
+function changeLast(arr, change) {
+  arr.splice(arr.length - 1, 1, change);
+}
 
+function changeAllValuesTo(arr, change) {
+  for(let i = 0; i < arr.length; i++) {
+    arr[i] = change;
+  }
+}
+
+function oddOrEven(arr) {
+  for(let i = 0; i <arr.length; i++) {
+    let isOdd = arr[i] % 2 !== 0 ? true : false;
+    if(isOdd) {
+      arr[i] = "odd";
+    }
+
+    else {
+      arr[i] = "even";
+    }
+  }
+}
+
+function changeNextThreeToValue(startIndex, arr, newValue) {
+  for(let i = startIndex; i < startIndex + 3; i++) {
+    arr[i] = newValue;
+  }
+}
 
 
 /*********************************
